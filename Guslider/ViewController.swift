2338e89f04f38a4edb0510e96cfd1407376092c1
Ricardo Marcelo Suman Júnior <ricardosuman@ricardos-macbook-pro.local>
2016-04-19 14:19:04 -0300

    Update view

--- Guslider/ViewController.swift
+++ Guslider/ViewController.swift
@@ -13,14 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let rect = CGRect(x: 0, y: 0, width: 300, height: 450)
+        //let rect = CGRect(x: view.frame.size.width - , y: 0, width: 300, height: 450)
         
         guard let firstImage = UIImage(named: "photo"),
             let secondImage = UIImage(named: "draw") else { return }
         
+        let rect = CGRect(x: (view.frame.size.width - firstImage.size.width) / 2, y: 0, width: 300, height: 450)
+        
         let slider = Guslider(frame: rect, firstImage: firstImage, secondImage: secondImage)
         
-        slider.center = view.center
+        //slider.center = view.center
         
         view.addSubview(slider)
     }
